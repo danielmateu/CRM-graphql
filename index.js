035f40acf8b4d0ec4c6d29f48f8982fe63c06556
Daniel Mateu <danielmateu86@gmail.com>
2023-04-24 16:38:29 +0200

    moviendo types y resolvers

--- index.js
+++ index.js
@@ -1,52 +1,7 @@
-const { ApolloServer, gql } = require('apollo-server');
+const { ApolloServer } = require('apollo-server');
 const { ApolloServerPluginLandingPageGraphQLPlayground } = require('apollo-server-core');
-
-// Schema
-const typeDefs = gql`
-
-    type Curso {
-        titulo: String
-        tecnologia: String
-    }
-
-    type Tecnologia {
-        tecnologia: String
-    }
-
-    type Query {
-        obtenerCursos: [Curso]
-        obtenerTecnologia: [Tecnologia]
-    }
-`
-
-const cursos = [
-    {
-        titulo: 'JavaScript Moderno Guía Definitiva Construye +10 Proyectos',
-        tecnologia: 'JavaScript ES6',
-    },
-    {
-        titulo: 'React – La Guía Completa: Hooks Context Redux MERN +15 Apps',
-        tecnologia: 'React',
-    },
-    {
-        titulo: 'Node.js – Bootcamp Desarrollo Web inc. MVC y REST API’s',
-        tecnologia: 'Node.js'
-    },
-    {
-        titulo: 'ReactJS Avanzado – FullStack React GraphQL y Apollo',
-        tecnologia: 'React'
-    }
-];
-
-
-// Resolvers
-const resolvers = {
-    Query: {
-        obtenerCursos: () => cursos, 
-        obtenerTecnologia: () => cursos
-    }
-}
-
+const typeDefs = require('./db/schema');
+const resolvers = require('./db/resolvers');
 
 // Servidor
 const server = new ApolloServer({
@@ -56,9 +11,6 @@ const server = new ApolloServer({
         ApolloServerPluginLandingPageGraphQLPlayground(),
     ]
 })
-
-// Conectar Schema con Resolvers
-
 
 // Arrancar el servidor
 server.listen().then(({ url }) => {
