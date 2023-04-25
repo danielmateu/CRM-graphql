02a3e9fafaa7f51c51f1735cad40ce4670f0192d
Daniel Mateu <danielmateu86@gmail.com>
2023-04-25 11:54:07 +0200

    Insertando usuarios en DB+ instalación bcrypt para hasheo de password

--- db/resolvers.js
+++ db/resolvers.js
@@ -1,11 +1,37 @@
-
+const Usuario = require('../config/models/Usuario');
 
 // Resolvers
 const resolvers = {
     Query: {
         // obtenerTecnologia: () => cursos
         obtenerCurso: () => 'GraphQL'
-    }
+    },
+    Mutation: {
+        nuevoUsuario: async (_, { input }) => {
+
+            const { email, password } = input;
+
+            // Revisar si el usuario ya está registrado
+            const existeUsuario = await Usuario.findOne({ email });
+            if (existeUsuario) {
+                throw new Error('El usuario ya está registrado');
+            }
+
+            // Hasear el Password
+            const salt = await bcryptjs.genSalt(10); 
+
+
+            try {
+                const usuario = new Usuario(input);
+                // Guardarlo en la base de datos
+                usuario.save(); // Guardarlo
+                return usuario;
+            } catch (error) {
+                console.log(error);
+            }
+        }
+    },
+
 }
 
 module.exports = resolvers;
