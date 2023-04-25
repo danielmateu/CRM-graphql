57b25380a104914db34cef633ce29ab28451acaf
Daniel Mateu <danielmateu86@gmail.com>
2023-04-25 12:19:02 +0200

    Creación del JWT

--- db/resolvers.js
+++ db/resolvers.js
@@ -1,11 +1,23 @@
 const Usuario = require('../config/models/Usuario');
 const bcryptjs = require('bcryptjs');
+const jwt = require('jsonwebtoken');
+
+// Helpers
+const crearToken = (usuario, secreta, expiresIn) => {
+    // console.log(usuario);
+    const { id, email, nombre, apellido } = usuario;
+
+    return jwt.sign({ id, email, nombre, apellido }, secreta, { expiresIn })
+}
 
 // Resolvers
 const resolvers = {
     Query: {
         // obtenerTecnologia: () => cursos
-        obtenerCurso: () => 'GraphQL'
+        obtenerUsuario: async (_, { token }) => {
+            const usuarioId = await jwt.verify(token, process.env.SECRETA);
+            return usuarioId;
+        }
     },
     Mutation: {
         nuevoUsuario: async (_, { input }) => {
@@ -19,9 +31,8 @@ const resolvers = {
             }
 
             // Hasear el Password
-            const salt = await bcryptjs.genSalt(10); 
+            const salt = await bcryptjs.genSalt(10);
             input.password = await bcryptjs.hash(password, salt);
-
 
             try {
                 const usuario = new Usuario(input);
@@ -30,6 +41,27 @@ const resolvers = {
                 return usuario;
             } catch (error) {
                 console.log(error);
+            }
+        },
+        autenticarUsuario: async (_, { input }) => {
+
+            const { email, password } = input;
+
+            // Si el usuario existe
+            const existeUsuario = await Usuario.findOne({ email });
+            if (!existeUsuario) {
+                throw new Error('El usuario no existe');
+            }
+
+            // Revisar si el password es correcto
+            const passwordCorrecto = await bcryptjs.compare(password, existeUsuario.password);
+            if (!passwordCorrecto) {
+                throw new Error('El password es incorrecto');
+            }
+
+            // Crear el token
+            return {
+                token: crearToken(existeUsuario, process.env.SECRETA, '24h')
             }
         }
     },
