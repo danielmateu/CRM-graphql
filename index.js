045af4d14063af14d4fdad714c1e316b4f14defa
Daniel Mateu <danielmateu86@gmail.com>
2023-04-27 13:29:57 +0200

    Configurando apollo para obtener al usuario autenticado

--- index.js
+++ index.js
@@ -6,26 +6,25 @@ const conectarDB = require('./config/db');
 const jwt = require('jsonwebtoken');
 require('dotenv').config({ path: 'variables.env' });
 
-
 // Conectar a la base de datos
 conectarDB();
 
 // Servidor
 const server = new ApolloServer({
 
-
     typeDefs,
     resolvers,
     context: ({ req }) => {
 
-        // console.log(req.headers['authorization']);
         console.log(req.headers);
+        console.log(req.headers['authorization']);
+
         const token = req.headers['authorization'] || '';
 
         if(token){
             try{
                 const usuario = jwt.verify(token.replace('Bearer ', ''), process.env.SECRETA);
-                console.log(usuario);
+                // console.log(usuario);
                 return {
                     usuario
                 }
